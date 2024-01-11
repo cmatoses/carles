2197f991514044793b6dfa93888eb0e0d762b510
carles matoses <carlesmatosesmiret@gmail.com>
2024-01-11 19:48:29 +0100

    eliminamos ficheros que sobran

--- carles_matoses_SQL_Data_Warehousing/5_carles_matoses_creacion_funcion_limpieza_null.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION keepcoding.clean_integer(value STRING) 
-RETURNS INT64 AS
-(( SELECT 
-      CASE
-        WHEN value IS NULL THEN -999999
-      ELSE
-        SAFE_CAST(value AS INT64)
-      END
-))
